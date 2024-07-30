45a37188ff93c2fc5906aa47ff81e75e874c121f
WongilK <wwwkim99@naver.com>
2024-07-30 15:27:15 +0900

    Docs : tailwind config에 color, fontSize 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,35 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      }
     },
+    colors: {
+      grey0: "F8F9FA",
+      grey1: "E9E9E9",
+      grey2: "E1E1E1",
+      grey3: "B7B7B7",
+      grey: "7C7C7C"
+    },
+    fontSize: {
+      extraLarge: "24px",
+      Large: "22px",
+      Medium: "16px",
+      small: "14px",
+      extraSmall: "12px"
+    }
   },
   plugins: [],
+  daisyui: {
+    themes: ["cmyk"]
+  }
 };
+
 export default config;
