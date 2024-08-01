f5c3d6601c862336f4d113bc0c98f76f088ee422
changchangwoo <changchangwoo@naver.com>
2024-08-01 14:54:43 +0900

    Comment : 모달 설명 주석 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-'use client';
-
-import { useModalStore } from "@/stores/modalStore";
-import { useStore } from "zustand";
-
 export default function Home() {
-  const { openModal } = useStore(useModalStore);
-
-
-
   return (
-    <main className="flex gap-5">
-      <button className="w-20 h-5 bg-slate-500 rounded-md" onClick={() => openModal("temp")}>모달 테스트 </button>
+    <main>
+      <p>Home</p>
     </main>
   );
 }
--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -10,35 +10,36 @@ interface ModalProps {
 }
 
 const MODAL_WIDTH = {
-  small : "335px",
-  medium : "514px",
-  large : "691px"
-}
+  small: "335px",
+  medium: "514px",
+  large: "691px",
+};
 
 const Modal = ({ size, title, children, closeModal }: ModalProps) => {
   const getModalSize = () => {
     switch (size) {
       case "small":
-        return `w-[${MODAL_WIDTH.small}]`;
+        return MODAL_WIDTH.small;
       case "medium":
-        return `w-[${MODAL_WIDTH.medium}]`;
+        return MODAL_WIDTH.medium;
       case "large":
-        return `w-[${MODAL_WIDTH.large}]`;
+        return MODAL_WIDTH.large;
       default:
-        return "";
+        return "auto";
     }
   };
 
   return (
     <div
-      className={`bg-white rounded p-5 ${getModalSize()} relative max-h-[980px] overflow-y-auto`}
+      style={{ width: getModalSize() }}
+      className={`bg-white rounded p-5 relative max-h-[90vh] overflow-y-auto`}
     >
       <div className="flex justify-between items-center mb-4">
         {size !== "small" && (
           <div className="mt-[10px] font-bold text-extraLarge">{title}</div>
         )}
         <IoClose
-          className="top-[10px] right-[10px] absolute w-6 h-6"
+          className="top-[10px] right-[10px] absolute w-6 h-6 cursor-pointer transition-transform duration-200 hover:scale-110"
           style={{ color: "#063D60" }}
           onClick={closeModal}
         />
