967278ac169ee1aa89cb4303af2d4fd19bcb77c6
changchangwoo <changchangwoo@naver.com>
2024-08-04 00:34:16 +0900

    Feat : 카테고리/지역 드롭다운 UI 구현

--- src/components/search/SearchDropdown.tsx
+++ src/components/search/SearchDropdown.tsx
@@ -1,33 +1,77 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react'
-import { FaCaretDown } from 'react-icons/fa'
+import React, { useState, useRef, useEffect } from "react";
+import { FaCaretDown } from "react-icons/fa";
+import { MdNavigateNext } from "react-icons/md";
 
 interface SearchDropdownProps {
-    title: string,
-    contents: {
-        name: string,
-        id: number
-    }[]
+  title: string;
+  contents: {
+    name: string;
+    id: number;
+  }[];
 }
 
 const SearchDropdown = ({ title, contents }: SearchDropdownProps) => {
-  const [isClicked, setIsClicked] = useState(false);
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const [currentCategory, setCurrentCategory] = useState<string>(title);
+  const dropdownRef = useRef<HTMLDivElement>(null); // Create a ref for the dropdown
+
+  const handleDropdownToggle = () => {
+    setIsClicked((prev) => !prev);
+  };
+
+  const handleItemClick = (name: string) => {
+    setCurrentCategory(name);
+    setIsClicked(false);
+  };
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsClicked(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <div 
-      className='relative flex gap-[10px] cursor-pointer'
-      onClick={() => setIsClicked((prev) => !prev)}
-    >
-      <div className={`text-extraLarge font-extrabold ${isClicked ? 'text-primary' : 'text-black'}`}>{title}</div>
-      <FaCaretDown className={`w-6 h-6 ${isClicked ? 'text-primary' : 'text-black'}`}/>
-      {isClicked && (
-        <div className='absolute top-full p-5 mt-2 bg-white w-[369px] z-50 border border-solid border-grey2 rounded-lg'>
-          <h1 className='text-large'></h1>
-          <ul className='list-none p-2'>
-            {contents.map(content => (
-              <li key={content.id} className='p-2 hover:bg-gray-200'>
+    <div ref={dropdownRef} className="relative">
+      <div
+        className="relative flex gap-[10px] cursor-pointer "
+        onClick={handleDropdownToggle}
+      >
+        <div
+          className={`text-extraLarge font-extrabold ${
+            isClicked ? "text-primary" : "text-black"
+          }`}
+        >
+          {currentCategory}
+        </div>
+        <FaCaretDown
+          className={`w-6 h-6 ${isClicked ? "text-primary" : "text-black"}`}
+        />
+      </div>
+      {isClicked && ( // 드롭다운 클릭시 출력
+        <div className="absolute top-full p-5 mt-2 bg-white w-[369px] h-[315px] z-10 border border-solid border-grey2 rounded-lg">
+          <h1 className="text-large font-extrabold">{title}</h1>
+          <hr className="mt-5 mb-[10px] fill-grey3" />
+          <ul className="list-none p-2 h-[200px] overflow-y-auto">
+            {contents.map((content) => (
+              <li
+                key={content.id}
+                className="p-2 hover:bg-grey1 flex justify-between rounded-lg text-grey4"
+                onClick={() => {
+                  handleItemClick(content.name);
+                }}
+              >
                 {content.name}
+                <MdNavigateNext className="w-[15px] h-[20px] font-bold" />
               </li>
             ))}
           </ul>
@@ -35,6 +79,6 @@ const SearchDropdown = ({ title, contents }: SearchDropdownProps) => {
       )}
     </div>
   );
-}
+};
 
 export default SearchDropdown;
--- src/components/search/SearchEngineSection.tsx
+++ src/components/search/SearchEngineSection.tsx
@@ -3,6 +3,10 @@ import SearchDropdown from "./SearchDropdown";
 
 const CategoryContents = [
   {
+    name: "전체",
+    id : 0
+  },  
+  {
     name: "아이돌",
     id: 1
   },
@@ -27,12 +31,56 @@ const CategoryContents = [
     id: 6
   }
 ];
+
+const RegionContents = [
+  {
+    name: "전체",
+    id : 0
+  },  
+  {
+    name: "서울",
+    id: 1
+  },
+  {
+    name: "경기도",
+    id: 2
+  },
+  {
+    name: "충청도",
+    id: 3
+  },
+  {
+    name: "강원도",
+    id: 4
+  },
+  {
+    name: "경상도",
+    id: 5
+  },
+  {
+    name: "전라도",
+    id: 6
+  },
+  {
+    name: "제주도",
+    id: 6
+  },
+  {
+    name: "전라도",
+    id: 6
+  },
+
+
+
+]
 const SearchEngineSection = () => {
   return (
     <div className="px-5">
       <div className="flex gap-5">
-        <SearchDropdown title="카테고리 전체" contents={CategoryContents} />
+        <SearchDropdown title="카테고리" contents={CategoryContents} />
+        <SearchDropdown title="지역" contents={RegionContents} />
       </div>
+      
     </div>
   );
 };
