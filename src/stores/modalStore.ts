73ce42f98ac921af50ab9359d5b22b16f3641511
Ud293nBqda@outlook.kr <johndoe@example.com>
2024-08-05 20:57:26 +0900

    Feat: Zustand 스토어 추가
    - 기존 modalStore에 extraData 필드 추가
    - 셀렉션 추가 전용 스토어 추가

--- src/stores/modalStore.ts
+++ src/stores/modalStore.ts
@@ -5,6 +5,8 @@ import { devtools } from "zustand/middleware";
 interface IModalStore {
   isOpen: boolean;
   modalType : TmodalType
+  extraData : any;
+  setExtraData : (data : any) => void;
   openModal: (modalType : TmodalType) => void;
   closeModal: () => void;
 }
@@ -13,6 +15,8 @@ export const useModalStore = create<IModalStore>()(
   devtools((set) => ({
     isOpen: false,
     modalType : null,
+    extraData : null,
+    setExtraData : (data : any) => set({ extraData : data }),
     openModal: (modalType : TmodalType = null) => 
       set({ isOpen: true, modalType: modalType }),
     closeModal: () => set({ isOpen: false, modalType: null})
