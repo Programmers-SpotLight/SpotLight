eba932b7998b30be30b853e60de175621654200b
changchangwoo <changchangwoo@naver.com>
2024-08-06 19:09:47 +0900

    Feat : 페이지네이션 UI 구현

--- src/hooks/queries/useFetchSearchResult.ts
+++ src/hooks/queries/useFetchSearchResult.ts
@@ -2,7 +2,16 @@ import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { IsearchResult, TsortType } from "@/models/searchResult.model";
 import { fetchSearchResult } from "@/http/search";
 
-const useFetchSearchResult = (category_id : string, region_id : string, tags : string[], sort : TsortType) : UseQueryResult<IsearchResult> => {
+interface IuseFetchSearchResult {
+    category_id? : string,
+    region_id? : string,
+    tags? : string[],
+    sort ?: TsortType,
+    page? : string,
+    limit? : string
+}
+
+const useFetchSearchResult = ({category_id, region_id, tags, sort, page, limit} : IuseFetchSearchResult) : UseQueryResult<IsearchResult> => {
     return useQuery<IsearchResult>({
         queryKey: ['searchResult', category_id, region_id, tags, sort],
         queryFn : () => fetchSearchResult(category_id, region_id, tags, sort)
