633a8fb87a3b4bfc87a3d04a8e195e95d6b0cd28
WongilK <wwwkim99@naver.com>
2024-08-16 10:32:16 +0900

    FEAT : 비공개 처리

--- src/app/user/[userId]/layout.tsx
+++ src/app/user/[userId]/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import SearchLoading from "@/components/search/search-contents/SearchLoading";
 import UserNavigation from "@/components/user/my/UserNavigation";
+import PrivateUser from "@/components/user/other-user/PrivateUser";
 import UserInfoWidget from "@/components/user/UserInfoWidget";
 import { useFetchUserInfo } from "@/hooks/queries/useFetchUserInfo";
 import { usePathname } from "next/navigation";
@@ -15,19 +16,30 @@ export default function RootLayout({
   if (!userIdMatch) return null;
   const userId = userIdMatch[1];
   const isMyPage = true;
-  const {data, isLoading, isError} = useFetchUserInfo(userId)
+  const { data, isLoading, isError } = useFetchUserInfo(userId);
 
-  if(!data) return null;
-  if(isLoading) return <div className="w-[1024px] flex justify-center items-center"><SearchLoading height="full"/></div>
-  if(isError) return <div>에러페이지입니다</div>
+  if (!data) return null;
+  if (isLoading)
+    return (
+      <div className="w-[1024px] flex justify-center items-center">
+        <SearchLoading height="full" />
+      </div>
+    );
+  if (isError) return <div>에러페이지입니다</div>;
 
-  return (  
+  if (data.is_private)
+    return (
+      <div className="w-[1024px] h-[calc(100vh-260px)] mx-auto border border-solid border-grey2 bg-grey0">
+        <PrivateUser />
+      </div>
+    );
+  return (
     <div className="w-[1024px] flex flex-col m-auto border border-solid border-grey2 bg-grey0 ">
-        <UserInfoWidget {...data} isMyPage={isMyPage} userId={userId} />
-        <div className="flex">
-        {isMyPage && <UserNavigation/>}
+      <UserInfoWidget {...data} isMyPage={isMyPage} userId={userId} />
+      <div className="flex">
+        {isMyPage && <UserNavigation />}
         {children}
-        </div>
+      </div>
     </div>
   );
 }
