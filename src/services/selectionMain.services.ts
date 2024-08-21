60397f5345ee69d74fe840c8fdac1cbe6dd39063
changchangwoo <changchangwoo@naver.com>
2024-08-21 15:24:10 +0900

    Fix : 인기 셀렉션 중복 제거

--- src/services/selectionMain.services.ts
+++ src/services/selectionMain.services.ts
@@ -42,42 +42,10 @@ export const getPopularSelection = async () => {
       .whereNotIn("selection.slt_status", ["private", "delete"]);
 
     const results = await query;
-
-    if (results.length < 4) {
-      const remainingCount = 4 - results.length;
-
-      const additionalQuery = dbConnectionPool("selection")
-        .distinct("selection.slt_id")
-        .select(
-          "selection.*",
-          "user.user_nickname",
-          "user.user_img",
-          "selection_category.slt_category_name",
-          "selection_location_option.slt_location_option_name"
-        )
-        .join("user", "selection.user_id", "=", "user.user_id")
-        .join(
-          "selection_category",
-          "selection.slt_category_id",
-          "=",
-          "selection_category.slt_category_id"
-        )
-        .join(
-          "selection_location_option",
-          "selection.slt_location_option_id",
-          "=",
-          "selection_location_option.slt_location_option_id"
-        )
-        .whereNotIn("selection.slt_status", ["private", "delete"])
-        .limit(remainingCount);
-
-      const additionalResults = await additionalQuery;
-      return [...results, ...additionalResults];
-    }
-
+    
     return results;
   } catch (error) {
-    console.log(error);
+    console.log(error)
     throw new Error(`Failed to fetch search Result`); // Todo : Error 처리
   }
 };
