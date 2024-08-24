1a11a5f2358708779edefe81f24b5660e607be70
changchangwoo <changchangwoo@naver.com>
2024-08-24 20:05:48 +0900

    Fix : prerender Suspense Boundary 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,23 @@ import InterestingSection from "@/components/main/InterestingSection";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { fetchHandler } from "@/http/http";
+import { Suspense } from "react";
+import SearchLoading from "@/components/search/search-contents/SearchLoading";
 
 export default async function Home() {
   try {
-  const popularSelections = await fetchHandler('api/selections/popular', 600);
-  return (
-    <main className="w-[1086px] bg-grey0 border border-solid border-grey2 m-auto pt-10 flex flex-col gap-10 box-border h-auto">
-      <BannerSection />
-      <RecommendationSection />
-      <PopularSection selections={popularSelections} />
-      <InterestingSection />
-    </main>
-  );
-} catch (error) {
-  // Todo : 서버사이드 데이터 페칭 간 에러 발생 처리
-
-}
+    const popularSelections = await fetchHandler("api/selections/popular", 600);
+    return (
+      <main className="w-[1086px] bg-grey0 border border-solid border-grey2 m-auto pt-10 flex flex-col gap-10 box-border h-auto">
+        <Suspense fallback={<SearchLoading />}>
+          <BannerSection />
+          <RecommendationSection />{" "}
+          <PopularSection selections={popularSelections} />
+          <InterestingSection />
+        </Suspense>
+      </main>
+    );
+  } catch (error) {
+    return <div>오류가 발생했습니다. 다시 시도해 주세요.</div>;
+  }
 }
