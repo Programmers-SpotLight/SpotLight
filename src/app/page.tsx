4bf8e089f9a2d5adf7a395213f156b42391d772f
changchangwoo <changchangwoo@naver.com>
2024-08-26 03:50:31 +0900

    Remove : 주석제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,21 +12,20 @@ import PageError from "@/components/common/PageError";
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  return <PageError/>
-  // try {
-  //   const popularSelections = await fetchHandler("api/selections/popular", 1);
-  //   return (
-  //     <Suspense fallback={<PageLoading />}>
-  //       <main className="w-[1086px] bg-grey0 border border-solid border-grey2 m-auto pt-10 flex flex-col gap-10 box-border h-auto">
-  //         <BannerSection />
-  //         <RecommendationSection />
-  //         <PopularSection selections={popularSelections} />
-  //         <InterestingSection />
-  //       </main>
-  //     </Suspense>
-  //   );
-  // } catch (error) {
-  //   console.log(error)
-  //   return <PageError/>
-  // }
+  try {
+    const popularSelections = await fetchHandler("api/selections/popular", 600);
+    return (
+      <Suspense fallback={<PageLoading />}>
+        <main className="w-[1086px] bg-grey0 border border-solid border-grey2 m-auto pt-10 flex flex-col gap-10 box-border h-auto">
+          <BannerSection />
+          <RecommendationSection />
+          <PopularSection selections={popularSelections} />
+          <InterestingSection />
+        </main>
+      </Suspense>
+    );
+  } catch (error) {
+    console.log(error)
+    return <PageError/>
+  }
 }
