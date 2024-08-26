d6cdeb5a9d92b311c494abba5beccd2b1ff3700a
changchangwoo <changchangwoo@naver.com>
2024-08-26 19:52:40 +0900

    Remove : 콘솔 제거

--- src/app/api/selections/recommendations/route.ts
+++ src/app/api/selections/recommendations/route.ts
@@ -1,4 +1,3 @@
-import { getUserHashTag } from '@/http/user.api';
 import authOptions from '@/libs/authOptions';
 import { manualRecommendationQueue } from '@/libs/queue';
 import { Ihashtags } from '@/models/hashtag.model';
@@ -18,15 +17,12 @@ export async function GET(req : NextRequest) : Promise<NextResponse<any | ErrorR
             const hashtags = await getUserHashTags(session_userId)
             if(hashtags.length === 0) {
             data = await serviceRandomSelection();
-            console.log("해시태그 없는 경우, 랜덤 셀렉션")
             } else {
             await manualRecommendationQueue.add('recommendation', { userId: session_userId }); 
             data = await serviceRecommendationSelection(session_userId)
-            console.log("해시태그 있는 경우, 추천 셀렉션")
             }
         } else {
             data = await serviceRandomSelection();
-            console.log("로그인 하지 않는 경우, 랜덤 셀렉션")
         }
 
         const mappingResults = mapSearchResults(data);
